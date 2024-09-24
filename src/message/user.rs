8fb126eada77da8bf5020b5878ae61773c30cdc0
idrissa <utileidrissa@gmail.com>
2024-09-24 14:40:55 +0200

    update user file

--- src/message/user.rs
+++ src/message/user.rs
@@ -1,7 +1,7 @@
 use std::net::IpAddr;
 use uuid::Uuid;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 pub struct User {
 	name: String,
 	id: Uuid,
@@ -9,6 +9,7 @@ pub struct User {
 	contacts: Option<Vec<User>>,
 }
 
+#[derive(PartialEq)]
 enum UserErr {
 	UserNotFound,
 	FileNotFound,
@@ -29,25 +30,91 @@ impl User {
 		
 		Ok(new_user)
 	}
-	fn addfriend(&mut self, contact: Self) {
+	fn update_contact(&mut self) -> Result<(), UserErr>{
+		let users = user_file::get_contacts()?;
+		if users == None {
+			return Ok(());
+		} else if self.contacts == None {
+			self.contacts = users;
+			return Ok(());
+		}
+		for user in users.unwrap().iter() {
+			match self.search_by_id(user.id) {
+				Ok(_) => (),
+				Err(UserErr::UserNotFound) => {
+					self.contacts.as_mut().unwrap().push((*user).clone())
+				},
+				Err(err) => { return Err(err) },
+			}
+		}
+		Ok(())
+	}
+	fn addcontact(&mut self, new_contact: Self) -> Result<(), UserErr>{
 		if self.contacts == None {
-			self.contacts = Some(Vec::new()).unwrap().push(contact);
-		} else {
-			self.contacts.unwrap().push(contact);
+			self.contacts = Some(Vec::new());
+			self.contacts.as_mut().unwrap().push(new_contact);
+			return Ok(());
+		}
+	
+		match self.search_by_id(new_contact.id) {
+			Ok(_) => Ok(()),
+			Err(UserErr::UserNotFound) => {
+				self.contacts.as_mut().unwrap().push(new_contact);
+				Ok(())
+			},
+			Err(err) => Err(err),
 		}
 	}
-	fn search_by_name(name: String) -> Result<Self, UserErr> {
+	fn search_by_name(&mut self, name: String) -> Result<Self, UserErr> {
+		let contact = self.contacts.as_mut();
+
+		if contact == None {
+			return Err(UserErr::UserNotFound);
+		} else {
+			for user in contact.unwrap().iter() {
+				if user.name == name {
+					return Ok((*user).clone());
+				}
+			}
+			return Err(UserErr::UserNotFound);
+		}
+	}
+	fn search_by_id(&mut self, id: Uuid) -> Result<Self, UserErr> {
+		let contact = self.contacts.as_mut();
+
+		if contact == None {
+			return Err(UserErr::UserNotFound);
+		} else {
+			for user in contact.unwrap().iter() {
+				if user.id == id {
+					return Ok((*user).clone());
+				}
+			}
+			return Err(UserErr::UserNotFound);
+		}
+	}
+	fn search_by_ip(&mut self, ip: IpAddr) -> Result<Self, UserErr> {
 		let user_file = user_file::userfile_path()?;
+		let contact = self.contacts.as_mut();
 
-
+		if contact == None {
+			return Err(UserErr::UserNotFound);
+		} else {
+			for user in contact.unwrap().iter() {
+				if user.ip == ip {
+					return Ok((*user).clone());
+				}
+			}
+			return Err(UserErr::UserNotFound);
+		}
 	}
 	fn remove_by_name(&mut self, name: String) -> Result<(), UserErr>
 	{
 		let mut i = 0;
 
-		for _ in self.contacts.iter() {
-			if self.contacts[i].name == name {
-				self.contacts.remove(i);
+		for user in self.contacts.as_mut().unwrap().iter() {
+			if user.name == name {
+				self.contacts.as_mut().unwrap().remove(i);
 				return Ok(());
 			}
 			i += 1;
@@ -58,9 +125,9 @@ impl User {
 	{
 		let mut i = 0;
 
-		for _ in self.contacts.iter() {
-			if self.contacts[i].ip == ip {
-				self.contacts.remove(i);
+		for user in self.contacts.as_mut().unwrap().iter() {
+			if user.ip == ip {
+				self.contacts.as_mut().unwrap().remove(i);
 				return Ok(());
 			}
 			i += 1;
@@ -71,11 +138,10 @@ impl User {
 	{
 		let mut i = 0;
 
-		for _ in self.contacts.iter()
-		{
-			if self.contacts[i].id == id
+		for user in self.contacts.as_mut().unwrap().iter() {
+			if user.id == id
 			{
-				self.contacts.remove(i);
+				self.contacts.as_mut().unwrap().remove(i);
 				return Ok(());
 			}
 			i += 1;
@@ -119,7 +185,7 @@ use super::{User, UserErr};
 			}
 		}
 	}
-	pub fn get_users() -> Result<Option<Vec<User>>, UserErr> {
+	pub fn get_contacts() -> Result<Option<Vec<User>>, UserErr> {
 		let mut users = Vec::new();
 		let file_path = userfile_path()?;
 		let content: String = match fs::read_to_string(file_path) {
@@ -130,18 +196,21 @@ use super::{User, UserErr};
 		let content = content.split("\n");
 
 		for str in content {
-			let mut infos = str.trim().trim_matches('@').split(':');
-			if infos.clone().any(|str| str.is_empty()) || infos.clone().count() != 3 {
+			let mut infos = str.trim().split(':');
+			if infos.clone().any(|str| str.is_empty()) || infos.clone().count() != 4 {
+				continue ;
+			}
+			if infos.nth(0).unwrap() != "@" {
 				continue ;
 			}
 			let new_user = User
 			{
-				name: infos.nth(0).unwrap().to_string(),
-				id: match Uuid::parse_str(infos.nth(1).unwrap()) {
+				name: infos.nth(1).unwrap().to_string(),
+				id: match Uuid::parse_str(infos.nth(2).unwrap()) {
 					Ok(id) => id,
 					Err(_) => { continue ;},
 				},
-				ip: match infos.nth(2).unwrap().parse() {
+				ip: match infos.nth(3).unwrap().parse() {
 					Ok(ip) => ip,
 					Err(_) => {continue ;},
 				},
